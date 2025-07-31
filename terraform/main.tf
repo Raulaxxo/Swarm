78b773a92cab7d24a0e89cd24b17137a5dd7290e
Raul Silva <raulaxxo@gmail.com>
2025-07-30 20:48:18 -0400

    cambios en dicso tf ec2

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "Raxxo"
 }
 
 resource "aws_instance" "mis_maquinas" {
-  count                  = 3
+  count                  = 1
   ami                    = "ami-020cba7c55df1f615"
   instance_type          = "t2.micro"
   subnet_id              = "subnet-00ffe6a72054ab086"
@@ -14,6 +15,12 @@ resource "aws_instance" "mis_maquinas" {
     "sg-0af2298f8e3ae9edb",  # entremaquinas
     "sg-08c41a3de52d67366"   # launch-wizard-1
   ]
+
+  root_block_device {
+    volume_size           = 30
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 
   user_data = file("${path.module}/init.sh")
 
